fb591bd624007796d8b2d0e92bdd6323d45a61f1
ssrz1 <sajadraza00@gmail.com>
2018-09-24 12:45:21 +0500

    Add component main

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import main from './components/main'
+export default class App extends React.Component {
+  render() {
+    return (
+      
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+ 
+});
