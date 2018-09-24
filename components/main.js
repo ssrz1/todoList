70cda175e1c244746ab8bc0504ef31555c85e4eb
ssrz1 <sajadraza00@gmail.com>
2018-09-24 13:43:29 +0500

    up

--- components/main.js
+++ components/main.js
@@ -20,7 +20,13 @@ export default class Main extends React.Component {
 
         </ScrollView>
         <View style={styles.footer}>
-          <TextInput style={styles.textInput}> </TextInput>
+          <TextInput 
+             style={styles.textInput}
+             placeholder= 'notes'
+             placeholderTextColor= 'white'
+             underlineColorAndroid='transparent'>
+
+          </TextInput>
         </View>
 
 
@@ -31,12 +37,19 @@ export default class Main extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-   
+    flex:1,
   },
   header: {
-
+    backgroungColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomWidth: 10,
+    borderBottomColor: '#ddd'
   },
   headerText: {
-
+    color: 'white',
+    fontSize: 18,
+    padding: 26
   },
+  
 });
