a70e4f953db6cae06ccc9449007c4e1d4e4864b0
ssrz1 <sajadraza00@gmail.com>
2018-09-24 14:19:28 +0500

    add note

---
+++ components/Note.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { 
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+} from 'react-native';
+
+export default class Main extends React.Component {
+  render() {
+    return (
+      
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+   
+
+});
