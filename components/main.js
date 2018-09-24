996271b7b53620ac6c71a7c22d963ef37d64b81a
ssrz1 <sajadraza00@gmail.com>
2018-09-24 13:59:19 +0500

    styleSheet

--- components/main.js
+++ components/main.js
@@ -36,20 +36,57 @@ export default class Main extends React.Component {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex:1,
+   
+   container: {
+      flex:1,
   },
   header: {
-    backgroungColor: 'white',
+    backgroundColor: '#E91E63',
     alignItems: 'center',
-    justifyContent: 'center',
-    borderBottomWidth: 10,
-    borderBottomColor: '#ddd'
-  },
-  headerText: {
+    justifyContent:'center',
+    //borderBottomWidth: 10,
+   // borderBottomColor: '#ddd'
+},
+headerText: {
     color: 'white',
     fontSize: 18,
-    padding: 26
+    padding: 20
+},
+    scrollContainer: {
+      flex: 1,
+      marginBottom: 100
   },
-  
+    footer: {
+      position: 'absolute',
+      bottom: 0,
+      left: 0,
+      right: 0,
+      zIndex: 10
+},
+    textInput: {
+      alignSelf: 'stretch',
+      color: '#fff',
+      padding: 20,
+      backgroundColor: '#252525',
+      borderTopWidth:2,
+      borderTopColor: '#ededed'
+},
+    addButton: {
+      position: 'absolute',
+      zIndex: 11,
+      right: 20,
+      bottom: 90,
+      backgroundColor: '#E91E63',
+      width: 70,
+      height: 70,
+      borderRadius: 35,
+      alignItems: 'center',
+      justifyContent: 'center',
+      elevation: 8
+},
+addButtonText: {
+      color: '#fff',
+      fontSize: 24
+}
+
 });
